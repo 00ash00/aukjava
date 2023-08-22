134de986176c5fa3e394014bd632ce5f8bcf44a4
00ash00 <alexander.shyriaiev@auk.edu.ua>
2023-08-22 15:21:40 +0300

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world   !");
+        System.out.println("Hello world!");
+        System.out.println("My name is Alexander Shyriaiev. Welcome to my first Java program.");
     }
 }
