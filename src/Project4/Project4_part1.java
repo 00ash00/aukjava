515ccc9b697480de47ca59eab6f84bd8017a29e0
00ash00 <alexander.shyriaiev@auk.edu.ua>
2023-09-05 13:39:31 +0300

    1st commit

--- src/Project4/Project4_part1.java
+++ src/Project4/Project4_part1.java
@@ -2,7 +2,7 @@ package Project4;
 
 public class Project4_part1 {
     public static void main(String[] args) {
-
+        // while loop
         System.out.println("Using while loop:");
         int i = 1;
         while (i <= 10) {
@@ -11,38 +11,33 @@ public class Project4_part1 {
         }
         System.out.println();
 
-        //do-while loop
-        System.out.println("Using do-while loop:");
-        int j = 10;
-        do {
-            System.out.print(j + " ");
-            j--;
-        } while (j >= 1);
-        System.out.println(); // Newline for separation
-
-        //for loop
+        // increment
         System.out.println("Using for loop (increment):");
-        for (int k = 1; k <= 10; k++) {
-            System.out.print(k + " ");
+        for (int j = 1; j <= 10; j++) {
+            System.out.print(j + " ");
         }
         System.out.println();
 
-        //for loop (decrement)
+        // decrement
         System.out.println("Using for loop (decrement):");
-        for (int l = 10; l >= 1; l--) {
-            System.out.print(l + " ");
+        for (int k = 10; k >= 1; k--) {
+            if (k != 6 && k != 4) { // Skipping 6 and 4
+                System.out.print(k + " ");
+            }
         }
-        System.out.println(); // Newline for separation
+        System.out.println(); // eparation
 
-        // Iterate through a String
+        // Iterating through a String
         System.out.println("Iterating through a String:");
         String text = "Hello, World!";
-        for (int m = 0; m < text.length(); m++) {
-            System.out.println(text.charAt(m));
+        int index = 0;
+        while (index < text.length()) {
+            System.out.println(text.charAt(index));
+            index++;
         }
         System.out.println();
 
-        //nested loops
+        // nested loops
         System.out.println("Using nested loops:");
         for (int outer = 1; outer <= 3; outer++) {
             for (int inner = 1; inner <= 3; inner++) {
